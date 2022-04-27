53166600ffdc89e060c37a2b32c8e9b8b03a4a7f
Pavel U <pahan174@gmail.com>
2022-04-27 09:46:09 +0500

    добавил basename в URL

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,11 +3,11 @@ from rest_framework.routers import SimpleRouter
 from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewsSet
 
 router = SimpleRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
+router.register('posts', PostViewSet, basename='posts_url')
+router.register('groups', GroupViewSet, basename='groups_url')
 router.register(r'^posts/(?P<post_id>\d+)/comments',
-                CommentViewSet, basename='comment')
-router.register('follow', FollowViewsSet, basename='follow')
+                CommentViewSet, basename='comment_url')
+router.register('follow', FollowViewsSet, basename='follow_url')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
